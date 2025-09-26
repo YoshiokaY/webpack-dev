9fc98274eb0db7891033c26aff9b75cf06be5a9c
YoshiokaY <yoshioka.y@nadia.bz>
2025-09-26 18:22:45 +0900

    [update]改修 #7

--- src/_assets/js/app.js
+++ src/_assets/js/app.js
@@ -3,3 +3,8 @@ import { HogeScript } from "./class/sample.ts";
 window.addEventListener("load", () => {
   new HogeScript();
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const img = document.querySelector("#concept .section-title");
+  // console.log(img.innerHTML);
+});
